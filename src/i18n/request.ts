31552b40adb860b64fdf916986d2e20038826fc8
Rami <rami.hamdouche.training@gmail.com>
2025-09-12 10:36:00 +0100

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,1 +1,9 @@
+import {getRequestConfig} from 'next-intl/server';
+
+export default getRequestConfig(async ({locale}) => {
+  // Charge les messages selon la locale courante
+  return {
+    messages: (await import(`@/messages/${locale}.json`)).default
+  };
+});
 
